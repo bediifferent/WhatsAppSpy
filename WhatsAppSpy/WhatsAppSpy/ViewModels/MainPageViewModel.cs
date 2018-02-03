b20e0e4475cb85862767cf13bd5874826bf8493c
Dinesh <dinesh.kriishnan@gmail.com>
2018-02-03 12:28:43 +0530

    Removed unused reference

--- WhatsAppSpy/WhatsAppSpy/ViewModels/MainPageViewModel.cs
+++ WhatsAppSpy/WhatsAppSpy/ViewModels/MainPageViewModel.cs
@@ -4,16 +4,10 @@ namespace WhatsAppSpy.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        private string _whatsAppUrl;
-        public string WhatsAppUrl
-        {
-            get => _whatsAppUrl;
-            set => SetProperty(ref _whatsAppUrl, value);
-        }
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
-            Title = "Test";
+           
         }
     }
 }
